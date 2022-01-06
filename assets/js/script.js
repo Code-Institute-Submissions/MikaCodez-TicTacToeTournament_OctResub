f75ba8a2a4e638e0eea1f12a3c2c895945033f2d
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-01-06 11:51:37 +0000

    changes made to html and added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,79 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener('DOMContentLoaded', function()  {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    const playerDisplay = document.querySelector('.display-player');
-    const resetButton = document.querySelector('#reset');
-    const announcer = document.querySelector('.announcer');
+const container = document.getElementById("gamegridArea");
+const squareArray = [];
+let nextMove = "X";
 
-    let board = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
-    let isGameActive = true;
+//determine if game is over
 
-    const PLAYERX_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';
-    const TIE = 'TIE';
+function gameOver(message) {
+  document.getElementById("winner").innerHTML = message;
+  container.style.display = "none";
+  document.getElementById("gameOver").style.display = "block";
+}
 
+//determine if game is a tie
+function isDraw() {
+  let shouldReturn = true;
+  squareArray.forEach(({ state }) => {
+    if (state == "") shouldReturn = false;
+  });
+  return shouldReturn;
+}
 
-    /*
-        Indexes within the board
-        [0] [1] [2]
-        [3] [4] [5]
-        [6] [7] [8]
-    */
-
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    function handleResultValidation() {
-        let roundWon = false;
-        for (let i = 0; i <= 7; i++) {
-            const winCondition = winningConditions[i];
-            const a = board[winCondition[0]];
-            const b = board[winCondition[1]];
-            const c = board[winCondition[2]];
-            if (a === '' || b === '' || c === '') {
-                continue;
-            }
-            if (a === b && b === c) {
-                roundWon = true;
-                break;
-            }
-        }
-
-    if (roundWon) {
-            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
-            isGameActive = false;
-            return;
-        }
-
-    if (!board.includes(''))
-        announce(TIE);
+//determine who won the game
+function wonGame() {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squareArray[a].state !== "" &&
+      squareArray[a].state === squareArray[b].state &&
+      squareArray[a].state === squareArray[c].state
+    ) {
+      return true;
     }
+  }
+  return false;
+}
 
-    const announce = (type) => {
-        switch(type){
-            case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
-                break;
-            case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
-                break;
-            case TIE:
-                announcer.innerText = 'Tie';
-        }
-        announcer.classList.remove('hide');
+class ClassSquare {
+  constructor(element, index) {
+    this.element = element;
+    this.index = index;
+    this.state = "";
+  }
+  clicked() {
+    this.state = nextMove;
+    this.element.classList.remove("notClicked");
+    this.element.onclick = function () {
+      return false;
     };
+    this.element.querySelector("p").innerHTML = this.state;
+    if (wonGame()) return gameOver("the winner is player " + this.state);
+    if (isDraw()) return gameOver("it is a draw");
+    nextMove == "X" ? (nextMove = "O") : (nextMove = "X");
+  }
+}
 
-    const isValidAction = (tile) => {
-        if (tile.innerText === 'X' || tile.innerText === 'O'){
-            return false;
-        }
-
-        return true;
-    };
-
-    const updateBoard =  (index) => {
-        board[index] = currentPlayer;
-    };
-
-    const changePlayer = () => {
-        playerDisplay.classList.remove(`player${currentPlayer}`);
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        playerDisplay.innerText = currentPlayer;
-        playerDisplay.classList.add(`player${currentPlayer}`);
-    };
-
-    const userAction = (tile, index) => {
-        if(isValidAction(tile) && isGameActive) {
-            tile.innerText = currentPlayer;
-            tile.classList.add(`player${currentPlayer}`);
-            updateBoard(index);
-            handleResultValidation();
-            changePlayer();
-        }
-    };
-    
-    const resetBoard = () => {
-        board = ['', '', '', '', '', '', '', '', ''];
-        isGameActive = true;
-        announcer.classList.add('hide');
-
-        if (currentPlayer === 'O') {
-            changePlayer();
-        }
-
-        tiles.forEach(tile => {
-            tile.innerText = '';
-            tile.classList.remove('playerX');
-            tile.classList.remove('playerO');
-        });
-    };
-
-    tiles.forEach( (tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
-    });
-
-    resetButton.addEventListener('click', resetBoard);
-});
+for (let index = 0; index < 9; index++) {
+  const div = document.createElement("div");
+  div.classList.add("square", "notClicked");
+  const square = new ClassSquare(div, index);
+  div.onclick = function () {
+    square.clicked();
+  };
+  div.appendChild(document.createElement("p"));
+  container.appendChild(div);
+  squareArray.push(square);
+}
