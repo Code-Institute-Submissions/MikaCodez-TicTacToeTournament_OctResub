fac16221712787dbe9372a0471d089151325fcb2
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-01-10 11:20:03 +0000

    fix clickable bug 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const container = document.getElementById("gamegridArea");
 const squareArray = [];
 let nextMove = "X";
 const resetButton = document.querySelector('#restart');
-const square = Array.from(document.getElementById("square")); 
+const square = Array.from(document.getElementsByClassName("square")); 
 
 //determine if game is over
 
